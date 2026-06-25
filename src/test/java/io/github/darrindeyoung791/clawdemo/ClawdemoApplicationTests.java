c03477f243708a456c45dcf9b53dada47bcb6a84
darrindeyoung791 <darrindeyoung791@outlook.com>
2026-06-25 09:17:38 +0800

    Initial commit

---
+++ src/test/java/io/github/darrindeyoung791/clawdemo/ClawdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.darrindeyoung791.clawdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClawdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
